9da99eddcf49d645587c9a25971f57318db6efee
taylorpatti <taylorpatti@g.harvard.edu>
2020-10-25 09:45:42 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='pytorch_masks',
+      version='0.1',
+      description='Functions for efficient, large-scale, elementwise tensor-tensor comparisons with PyTorch Autograd support.',
+      url='https://github.com/taylorpatti/pytorch_masks',
+      author='Taylor Lee Patti',
+      author_email='taylorpatti@g.harvard.edu',
+      license='GNU LGPL',
+      packages=['pytorch_masks'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=[
+          'torch'
+      ],
+      zip_safe=False)
