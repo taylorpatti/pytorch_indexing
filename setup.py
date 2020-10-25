fa95b06726f6aae127e091d14eccec18bf05e62e
taylorpatti <taylorpatti@g.harvard.edu>
2020-10-25 09:52:58 -0400

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(name='pytorch_indexing',
       version='0.1',
       description='Functions for efficient, large-scale, elementwise tensor-tensor comparisons with PyTorch Autograd support.',
-      url='https://github.com/taylorpatti/pytorch_masks',
+      url='https://github.com/taylorpatti/pytorch_indexing',
       author='Taylor Lee Patti',
       author_email='taylorpatti@g.harvard.edu',
       license='GNU LGPL',
-      packages=['pytorch_masks'],
+      packages=['pytorch_indexing'],
       test_suite='nose.collector',
       tests_require=['nose'],
       install_requires=[
